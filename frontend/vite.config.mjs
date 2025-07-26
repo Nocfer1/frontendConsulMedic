3226c706df079eeb6abab4e0051fd3a2676ac4a7
Ignacio Essus <i.essus01@ufromail.cl>
2025-07-25 22:35:46 -0400

    Se añade configuración de pruebas en `vite.config.mjs` incluyendo soporte para `jsdom` y archivo de setup.

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -18,4 +18,9 @@ export default defineConfig({
             }
         }
     },
+    test: {
+        globals: true,
+        environment: 'jsdom',         //Recomendado para React
+        setupFiles: './src/setupTests.js'
+    },
 })
